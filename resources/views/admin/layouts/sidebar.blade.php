c89308733c726fb9e3feb59c7e12a2280cf1f147
iqbal <iqbal.khan@kk-lotto.com>
2022-07-19 14:24:12 +0700

    bug fix

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -10,10 +10,14 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
+        @if (!empty(Auth::user()->image)) 
           <img src="{{ asset('public/admin/images/user/'.Auth::user()->image) }}" class="img-circle elevation-2" alt="User Image">
+        @endif
         </div>
         <div class="info">
+        @if (!empty(Auth::user()->name)) 
           <a href="{{route('admin_profile')}}" class="d-block">{{ Auth::user()->name }}</a>
+        @endif
         </div>
       </div>
 
