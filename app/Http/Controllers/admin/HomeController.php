6afced8334c8f41efa8b8addd330899f4beda662
shah <shahniyaz@kk-lotto.com>
2022-07-26 11:20:08 +0700

    this is for testing

--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -23,7 +23,8 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-    {
+    { 
+        
         return view('admin.home');
     }
 }
