4904c55a14af96d256fe50d3ff4ab82556f48fda
shah <shahniyaz@kk-lotto.com>
2022-07-26 15:29:25 +0700

    dashboard changes

--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -22,10 +22,15 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     { 
-
-        
-        return view('admin.home');
+         
+         
+        $query = new Company;
+		$query = $query->Where('status','enabled');
+        $data = $query->orderBy('id', 'DESC')->paginate(DEFAULT_PAGINATION_LIMIT);
+        $selectBrand = $request->brands;
+       // dd($query);
+        return view('admin.home')->with(array('company'=>$data,'selectCountry'=>$selectBrand));
     }
 }
