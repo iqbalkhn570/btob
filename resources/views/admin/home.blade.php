f0b91d4778eaca72797e31c3e8b4269c729942c0
shah <shahniyaz@kk-lotto.com>
2022-07-27 16:07:27 +0700

    dashboard pie chart add

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -8,27 +8,25 @@
           <div class="col-sm-8">
             <h1 class="m-0">Dashboard v2</h1>
           </div><!-- /.col -->
-         
-          
-         
+
           <div class="col-sm-4">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Dashboard v2</li>
             </ol>
           </div><!-- /.col -->
-        
+
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
 
-    
+
 
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
-        
+
       <div class="card card-default">
           <div class="card-header">
             <h3 class="card-title">Filters</h3>
@@ -46,33 +44,33 @@
           <div class="card-body">
           <form class="" method="get" action="" id="search_brands" >
             <div class="row">
-            
+
               <div class="col-md-2">
                  <div class="form-group">
                  <label>Companies</label>
                        <select class="form-control" name="brands" onchange="this.form.submit()">
                        @foreach($company as $row)
-                  
+
                       <option value="{{ $row->name }}" {{ $selectCountry == $row->name ? 'selected' : '' }}>{{ $row->name }}</option>
                       @endforeach
-                  
+
                        </select>
                      </div>
-                
+
               </div>
               <!-- /.col -->
               <div class="col-md-3">
-                
-               
+
+
               </div>
               <!-- /.col -->
-              
+
             </div>
             <!-- /.row -->
-            </form> 
-           
+            </form>
+
           </div>
-          
+
         </div>
         <div class="row">
           <div class="col-lg-3 col-6">
@@ -137,8 +135,142 @@
           <!-- ./col -->
         </div>
         <!-- /.row -->
-        
+
+     
+
         <div class="row">
+          <section class="col-lg-8 connectedSortable">
+          <!-- solid sales graph -->
+           <div class="card bg-gradient-info">
+                  <div class="card-header border-0">
+                    <h3 class="card-title">
+                      <i class="fas fa-th mr-1"></i>
+                      Turnover Graph
+                    </h3>
+
+                    <div class="card-tools">
+                      <button type="button" class="btn bg-info btn-sm" data-card-widget="collapse">
+                        <i class="fas fa-minus"></i>
+                      </button>
+                      <button type="button" class="btn bg-info btn-sm" data-card-widget="remove">
+                        <i class="fas fa-times"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <canvas class="chart" id="line-chart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                  </div>
+                  <!-- /.card-body -->
+                  <div class="card-footer bg-transparent">
+                    <div class="row">
+                      <div class="col-4 text-center">
+                        <input type="text" class="knob" data-readonly="true" value="20" data-width="60" data-height="60"
+                              data-fgColor="#39CCCC">
+
+                        <div class="text-white">Mail-Orders</div>
+                      </div>
+                      <!-- ./col -->
+                      <div class="col-4 text-center">
+                        <input type="text" class="knob" data-readonly="true" value="50" data-width="60" data-height="60"
+                              data-fgColor="#39CCCC">
+
+                        <div class="text-white">Online</div>
+                      </div>
+                      <!-- ./col -->
+                      <div class="col-4 text-center">
+                        <input type="text" class="knob" data-readonly="true" value="30" data-width="60" data-height="60"
+                              data-fgColor="#39CCCC">
+
+                        <div class="text-white">In-Store</div>
+                      </div>
+                      <!-- ./col -->
+                    </div>
+                    <!-- /.row -->
+                  </div>
+                  <!-- /.card-footer -->
+                </div>
+                <!-- /.card -->
+
+
+
+          </section>
+
+
+       <section class="col-lg-4 connectedSortable">
+       <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Browser Usage</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-md-8">
+                    <div class="chart-responsive">
+                      <canvas id="pieChart" height="150"></canvas>
+                    </div>
+                    <!-- ./chart-responsive -->
+                  </div>
+                  <!-- /.col -->
+                  <div class="col-md-4">
+                    <ul class="chart-legend clearfix">
+                      <li><i class="far fa-circle text-danger"></i> Chrome</li>
+                      <li><i class="far fa-circle text-success"></i> IE</li>
+                      <li><i class="far fa-circle text-warning"></i> FireFox</li>
+                      <li><i class="far fa-circle text-info"></i> Safari</li>
+                      <li><i class="far fa-circle text-primary"></i> Opera</li>
+                      <li><i class="far fa-circle text-secondary"></i> Navigator</li>
+                    </ul>
+                  </div>
+                  <!-- /.col -->
+                </div>
+                <!-- /.row -->
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer p-0">
+                <ul class="nav nav-pills flex-column">
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">
+                      United States of America
+                      <span class="float-right text-danger">
+                        <i class="fas fa-arrow-down text-sm"></i>
+                        12%</span>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">
+                      India
+                      <span class="float-right text-success">
+                        <i class="fas fa-arrow-up text-sm"></i> 4%
+                      </span>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">
+                      China
+                      <span class="float-right text-warning">
+                        <i class="fas fa-arrow-left text-sm"></i> 0%
+                      </span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <!-- /.footer -->
+            </div>
+       </section>
+
+
+       </div>
+
+       <div class="row">
           <div class="col-lg-3 col-6">
             <!-- small box -->
             <div class="small-box bg-info">
@@ -200,61 +332,6 @@
           </div>
           <!-- ./col -->
         </div>
-        <!-- /.row -->
-
-       <!-- solid sales graph -->
-       <div class="card bg-gradient-info">
-              <div class="card-header border-0">
-                <h3 class="card-title">
-                  <i class="fas fa-th mr-1"></i>
-                  Sales Graph
-                </h3>
-
-                <div class="card-tools">
-                  <button type="button" class="btn bg-info btn-sm" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn bg-info btn-sm" data-card-widget="remove">
-                    <i class="fas fa-times"></i>
-                  </button>
-                </div>
-              </div>
-              <div class="card-body">
-                <canvas class="chart" id="line-chart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
-              </div>
-              <!-- /.card-body -->
-              <div class="card-footer bg-transparent">
-                <div class="row">
-                  <div class="col-4 text-center">
-                    <input type="text" class="knob" data-readonly="true" value="20" data-width="60" data-height="60"
-                           data-fgColor="#39CCCC">
-
-                    <div class="text-white">Mail-Orders</div>
-                  </div>
-                  <!-- ./col -->
-                  <div class="col-4 text-center">
-                    <input type="text" class="knob" data-readonly="true" value="50" data-width="60" data-height="60"
-                           data-fgColor="#39CCCC">
-
-                    <div class="text-white">Online</div>
-                  </div>
-                  <!-- ./col -->
-                  <div class="col-4 text-center">
-                    <input type="text" class="knob" data-readonly="true" value="30" data-width="60" data-height="60"
-                           data-fgColor="#39CCCC">
-
-                    <div class="text-white">In-Store</div>
-                  </div>
-                  <!-- ./col -->
-                </div>
-                <!-- /.row -->
-              </div>
-              <!-- /.card-footer -->
-            </div>
-            <!-- /.card -->
-
-        
-      </div><!--/. container-fluid -->
     </section>
     <!-- /.content -->
 @endsection
