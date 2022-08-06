f69330c74e2998ff84f3420bcb90cf6d5a440bc9
Phen <phenplot1996@gmail.com>
2022-08-06 11:21:38 +0700

    lotter store

--- app/Http/Controllers/Api/LotteryController.php
+++ app/Http/Controllers/Api/LotteryController.php
@@ -7,7 +7,11 @@ use App\Http\Controllers\Api\BaseController as BaseController;
 use App\Models\Lottery;
 use Validator;
 use App\Http\Resources\LotteryResource;
-     
+use Faker\Core\Uuid;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
 class LotteryController extends BaseController
 {
     /**
@@ -29,20 +33,74 @@ class LotteryController extends BaseController
      */
     public function store(Request $request)
     {
-        $input = $request->all();
-        print_r($input);die;
-     
-        $validator = Validator::make($input, [
-            //'number_pattern' => 'required',
+        $payLoad = json_decode(request()->getContent(), true);
+    
+        if(! $payLoad){
+            return $this->sendError('Validation Error.');       
+        }
+        $validator = Validator::make($payLoad, [
+            'dates' => 'required|array', //Must be a number and length of value is 8
+            'games' => 'required|array',
+            'datas' => 'required|array'
         ]);
      
         if($validator->fails()){
             return $this->sendError('Validation Error.', $validator->errors());       
         }
+        $customerId = 5;
+        $companyId = 2;
+
+        DB::beginTransaction();
+        try {
+            //code...
+            $dates = $payLoad['dates'];
+            $games = $payLoad['games'];
+            $datas = $payLoad['datas'];
+            $referenceNum = Str::uuid()->toString().'/'.Carbon::now()->year.'/REF';
+            foreach ($datas as $key => $value) {
+                # code...
+                $lotteryData['reference_number'] = $referenceNum;
+                $lotteryData['customer_id'] = $customerId;
+                $lotteryData['company_id'] = $companyId;
+                $lotteryData['number_pattern'] = @$value['number'];
+                $lotteryData['big_bet_amount'] = @$value['big_bet'];
+                $lotteryData['small_bet_amount'] = @$value['small_bet'];
+
+                $boxType = '2';
+                if( @$value['box'] )
+                    $boxType = '0';
+                elseif( @$value['ibox'] )
+                    $boxType = '1';
+                $lotteryData['bet_type'] = $boxType;
+
+                $lotteryData['total_amount'] = @$value['amount'];
+
+                $lottery = Lottery::create($lotteryData);
+                
+                //save customer lotteries slave
+                foreach ($games as $key => $game) {
+                    foreach ($dates as $key => $date) {
+                        # code...
+                        $slaveData['customer_lottery_id'] = $lottery->id;
+                        $slaveData['game_id'] = $game;
+                        $formatedDate = Carbon::createFromFormat('l, d M',$date)->format('Y-m-d');
+                        $slaveData['game_date'] = $formatedDate;
+
+                        $slaveData['lottery_number'] = $lottery->number_pattern;
+                        DB::table('customer_lotteries_slave')->insert($slaveData);
+                    }
+                    
+                }
+            }
+
+        } catch (\Throwable $th) {
+            return $this->sendError('Store data error.',$th);       
+
+        }
+        DB::commit();
+
      
-        $lottery = Lottery::create($input);
-     
-        return $this->sendResponse(new LotteryResource($lottery), 'Lottery created successfully.');
+        return $this->sendResponse([], 'Lottery created successfully.');
     } 
    
    
